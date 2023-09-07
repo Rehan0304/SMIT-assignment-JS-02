3302e2c9a7000be513fa0c73eea78daddb9acfee
Muhammad rehan <109660304+Rehan0304@users.noreply.github.com>
2023-09-08 04:50:44 +0500

    js assignment ch 5

---
+++ index.js
@@ -0,0 +1,110 @@
+//! chapter 05
+
+//? Q 01
+// var a=5;
+// var b=3;
+// var sum=a+b;
+// document.write(`sum of ${a} and ${b} is ${sum} <br>`);
+//? Q 02
+// var prod=a*b;
+// document.write(`Product of ${a} and ${b} is ${prod}<br>`)
+// var subtract=a-b;
+// document.write(`substraction of ${a} and ${b} is ${subtract}<br>`)
+// var divide=a/b;
+// document.write(`division of ${a} and ${b} is ${divide}<br>`)
+// var mod=a%b;
+// document.write(`modoulus of ${a} and ${b} is ${mod}<br>`)
+
+//? Q 03
+
+// var a;
+// document.write(`value after variable declaretion is  ${a}<br>`);
+// a=5;
+// document.write(`initial value is ${a}<br>`);
+// a=++a;
+// document.write(`value after increment is ${a}<br>`);
+// a=a+7;
+// document.write(`value after addition is ${a}<br>`);
+// a=--a;
+// document.write(`value after decrement ${a}<br>`);
+// a=a%3;
+// document.write(`value of mod after dividing by 3 is ${a}<br>`);
+
+//? Q 04
+
+// var costOfOneMovieTicket=600;
+// var costFor5Ticket=costOfOneMovieTicket*5;
+// document.write(`total cost to buy 5 tickets for movie is ${costFor5Ticket}PKR`);
+
+//? Q 05
+
+// var table=`Table of 4`;
+// document.write(table) ;
+// var j=4;
+// for(let i=1; i<=10; i++){
+//     // document.write(i);
+//     var prod=j*i;
+//     document.write(`<br> 4 X ${i} = ${prod} <br>`);
+
+// }
+
+//? Q 06
+
+// var C=prompt(`Enter temperature in celsius`);
+// var F=prompt(`Enter temperature in Fahrenheit`);
+
+// var tempInF=(C*9/5)+32;
+// document.write(`${C}C is ${tempInF}F <br>`);
+// var tempInC=(F-32)*5/9;
+// document.write(`${F}F is ${tempInC}C`);
+
+//? Q 07
+
+// var item01Price=650;
+// var item02Price=100;
+// var item01Quantity=3;
+// var item02Quantity=7;
+// var shipCharge=100
+// document.write(`price of item 1 is ${item01Price}<br> quantity of item 1 is ${item01Quantity}<br> price of item 2 is ${item02Price}<br> quantity of item 02 is ${item02Quantity}<br> shoipping charges ${shipCharge}<br>`);
+// var totalCost=(item01Price*item01Quantity)+ (item02Price*item02Quantity)+shipCharge;
+// document.write(`Total cost of your order is ${totalCost}`);
+
+//? Q 08
+
+// var totalMarks= 980;
+// var obtainedMarks=804;
+// var percentage=obtainedMarks/totalMarks*100;
+// document.write(`Total marks:${totalMarks}<br> Obtained marks:${obtainedMarks}<br> percentage:${percentage}`)
+
+//? Q 09
+
+// var dollar=10; riyal=25;
+// var inPakistanirupee=(dollar*104.80) + (riyal*28);
+// document.write(`Total currency:${inPakistanirupee}`)
+
+//? Q 10
+
+// var num=5;
+// var calculation=((num+5)*10)/2;
+// document.write(calculation);
+
+//? Q 11
+
+// var currentYear = prompt(`enter thr current year`);
+// var birthYear = prompt(`enter your birth year`);
+// var age = currentYear - birthYear;
+// document.write(`Current year:${currentYear}<br> Birth year:${birthYear}<br>
+// Your age:${age}`);
+
+//? Q 11
+
+// var radius = prompt(`please enter radius of circle`);
+// document.write(`Radius of circle is:${radius}<br>`);
+// var pi=3.142;
+// var circumference=2*pi*radius;
+// document.write(`The circumference is:${circumference}<br>`);
+// var area=pi*(radius**2);
+// document.write(`The area is:${area}`);
+
+//? Q 13
+
